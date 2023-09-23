93d04997611b0174febcd138a8ad2e8c7fe65eaf
tendsto0 <2msapta2002@gamil.com>
2023-09-23 14:24:21 +0530

    changeing 7==7 to true

--- pi01.py
+++ pi01.py
@@ -1,15 +1,15 @@
 from random import random
 from math import sqrt
-count =0
+count = 0
 n = 0;
-while(7==7):
+while(True):
     x = random()*2 - 1;
     y = random()*2 - 1;
     d = sqrt(x**2 + y**2)
     if d <= 1:
-        count +=1
+        count += 1
     n+=1
-    print(n,4*count/n)
+    print(n, 4*count/n)
     
     
     
